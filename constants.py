f0566ecbf2392c74c410747656aaf2933f4609eb
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2023-11-30 07:37:27 +0100

    added skel constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+# NOTE: THIS FILE SHOULD NOT BE PUSED TO GITHUB
+# Enter your API keys below
+# Only services you want to use are required
+
+
+# Zerotier API Keys (required)
+CONTROLLER_TOKEN = "YOUR TOKEN HERE"
+NODEID = "YOUR NODE ID HERE"
+NWID = "YOUR NETWORK ID HERE"
+
+# Discord API Keys (optional)
+DISCORD_TOKEN = ""
+
+# Slack API Keys (optional)
+SLACK_TOKEN = ""
+SLACK_SIGNING_SECRET = ""
+
+# Google Chat Keys (optional)
+GOOGLE_CHAT_KEY = ""
